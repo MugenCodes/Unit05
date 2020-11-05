8fed79534fe72c1df0d3fccab641484e831e4a84
viewtifult <tashanduncan1994@gmail.com>
2020-11-05 21:53:45 +0000

    added own search functionality to app.js and removed preset search script

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 baguetteBox.run('.gallery');
 
 
-const search = new Filter('search', 'data-caption');
+function myFunction() {
+    // Declare variables
+    
+    const input = document.getElementById('search');
+    const filter = input.value.toUpperCase();
+    const image = document.getElementsByTagName("div");
+    
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (let i = 0; i < image.length; i++) {
+     const caption = document.getElementsByTagName('a')[i].getAttribute('data-caption')
+  
+      if (caption.toUpperCase().indexOf(filter) > -1) {
+        image[i].style.display = "";
+      } else {
+        image[i].style.display = "none";
+      }
+    }
+  }
